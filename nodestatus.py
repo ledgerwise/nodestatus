c2061c751bb8240d45b1c97d0c19684aa3a6dc03
Jorge Ferrando <jorfermo@gmail.com>
2020-09-03 14:29:21 +0200

    Add url check

--- nodestatus.py
+++ nodestatus.py
@@ -13,7 +13,7 @@ import json
 import datetime
 import eospy.cleos
 from tenacity import retry, stop_after_attempt, wait_fixed
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse
 from include.checker import Checker
 
 pp = pprint.PrettyPrinter(indent=4)
@@ -81,7 +81,9 @@ def get_producers(chain):
         CLEOS = eospy.cleos.Cleos(url=chain['api_node'])
         result = CLEOS.get_producers(limit=LIMIT)
 
-        isFIO = 'producers' in result
+        isFIO = chain[
+            'chain_id'] == '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c' or chain[
+                'chain_id'] == 'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e'
 
         if isFIO:
             producers = result['producers']
@@ -93,16 +95,20 @@ def get_producers(chain):
                                          lower_bound=result['more'])
             producers += result['rows']
 
-        active_producers = [{
-            'owner':
-            x['owner'],
-            'url':
-            x['url'],
-            'bp_json_url':
-            urljoin(x['url'], '/bp.json'),
-            'chains_json_url':
-            urljoin(x['url'], '/chains.json')
-        } for x in producers if x['is_active'] != 0]
+        active_producers = []
+        for producer in producers:
+            if producer['is_active'] != 0:
+                if not producer['url'].startswith('http'):
+                    producer['url'] = 'http://' + producer['url']
+                p = {
+                    'owner': producer['owner'],
+                    'url': producer['url'],
+                    'bp_json_url': urljoin(producer['url'], '/bp.json'),
+                    'chains_json_url': urljoin(producer['url'], '/chains.json')
+                }
+                if isFIO:
+                    p['fio_address'] = producer['fio_address']
+            active_producers.append(p)
 
         if chain[
                 'chain_id'] == '1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4':
@@ -148,71 +154,68 @@ def main():
         producers_array = []
         testnet_producers = []
 
+        isFIO = chain_info[
+            'chain_id'] == '21dcae42c0182200e93f954a074011f9048a7624c6fe81d3c9541a614a88bd1c' or chain[
+                'chain_id'] == 'b20901380af44ef59c5918439a1f9a41d83669020319a80574b804a5f95cbd7e'
+
         try:
             producers = get_producers(chain_info)
-            for testnet in chain_info['testnets']:
-                testnet_producers_array = get_producers(testnet)
-                testnet_producers += [
-                    x['owner'] for x in testnet_producers_array
-                ]
-            print(testnet_producers)
+            if not isFIO:
+                for testnet in chain_info['testnets']:
+                    testnet_producers_array = get_producers(testnet)
+                    testnet_producers += [
+                        x['owner'] for x in testnet_producers_array
+                    ]
         except Exception as e:
             logging.critical('Too many retries getting producers')
             continue
 
         for producer in producers:
+            # if producer['fio_address'] != 'bp@ledgerwise':
+            #     continue
             logging.info('Checking producer {}'.format(producer['owner']))
             checker = Checker(chain_info, producer, logging)
             checker.run_checks()
 
             #Check producer in testnet
-            if producer['owner'] not in testnet_producers:
-                msg = 'Producer {} might not be registered or registered with some other name in testnet'.format(
-                    producer['owner'])
-                logging.warning(msg)
-                checker.warnings.append(msg)
-            else:
-                msg = 'Producer name {} is registered as producer in testnet'.format(
-                    producer['owner'])
-                logging.info(msg)
-                checker.oks.append(msg)
+            if not isFIO:
+                if producer['owner'] not in testnet_producers:
+                    msg = 'Producer {} might not be registered or registered with some other name in testnet'.format(
+                        producer['owner'])
+                    logging.warning(msg)
+                    checker.warnings.append(msg)
+                else:
+                    msg = 'Producer name {} is registered as producer in testnet'.format(
+                        producer['owner'])
+                    logging.info(msg)
+                    checker.oks.append(msg)
 
             healthy_api_endpoints += checker.healthy_api_endpoints
             healthy_p2p_endpoints += checker.healthy_p2p_endpoints
             healthy_history_endpoints += checker.healthy_history_endpoints
             healthy_hyperion_endpoints += checker.healthy_hyperion_endpoints
-            producers_array.append({
-                'account':
-                producer['owner'],
-                'org_name':
-                checker.org_name,
-                'history':
-                len(checker.healthy_history_endpoints),
-                'hyperion':
-                len(checker.healthy_hyperion_endpoints),
-                'patroneos':
-                checker.patroneos,
-                'position':
-                checker.producer_info['position'],
-                'status':
-                checker.status,
-                'errors':
-                checker.errors,
-                'oks':
-                checker.oks,
-                'warnings':
-                checker.warnings,
-                'endpoint_errors':
-                checker.endpoint_errors,
-                'endpoint_oks':
-                checker.endpoint_oks,
-                'api_endpoints':
-                checker.api_endpoints,
-                'p2p_endpoints':
-                checker.p2p_endpoints,
-                'bp_json':
-                checker.producer_info['bp_json_url']
-            })
+
+            producer_info = {
+                'account': producer['owner'],
+                'org_name': checker.org_name,
+                'history': len(checker.healthy_history_endpoints),
+                'hyperion': len(checker.healthy_hyperion_endpoints),
+                'patroneos': checker.patroneos,
+                'position': checker.producer_info['position'],
+                'status': checker.status,
+                'errors': checker.errors,
+                'oks': checker.oks,
+                'warnings': checker.warnings,
+                'endpoint_errors': checker.endpoint_errors,
+                'endpoint_oks': checker.endpoint_oks,
+                'api_endpoints': checker.api_endpoints,
+                'p2p_endpoints': checker.p2p_endpoints,
+                'bp_json': checker.producer_info['bp_json_url']
+            }
+            if isFIO:
+                producer_info['fio_address'] = producer['fio_address']
+
+            producers_array.append(producer_info)
 
         healthy_api_endpoints = list(set(healthy_api_endpoints))
         healthy_p2p_endpoints = list(set(healthy_p2p_endpoints))
